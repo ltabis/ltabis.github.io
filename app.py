bac1252f7bc49105c2c05ceb6044e5b9f92c0f13
ltabis <tab.rysak70@gmail.com>
2020-06-15 21:56:00 +0200

    :alembic: Generating static templates using flask.

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from sys import stderr
+from backend.projects import create_project_templates
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+
+    project_templates = create_project_templates()
+
+    with open('./index.static.html', 'w') as file:
+        file.write(render_template('index.html', projects=project_templates))
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
---
+++ backend/projects.py
@@ -0,0 +1,42 @@
+from github import Github
+from flask import render_template, Markup
+from sys import stderr
+from os import getenv
+
+def create_project_templates():
+    """Create a template for each choosen project.
+
+    Returns:
+        Template[]: an array of html templates. 
+    """
+
+    auth = connect_to_github()
+
+    projects = [
+        "ltabis/electron-dev-environment",
+        "matteovol/EclataxEngine",
+        "ltabis/raytracer",
+        "ltabis/Utility-scripts",
+        "ltabis/MinecraftBackupTool",
+        "ltabis/toDoJAVA",
+    ]
+
+    template_array = []
+
+    for project in projects:
+
+        repo = auth.get_repo(project)
+
+        template_array.append(
+            Markup(render_template('project_card.html', title=repo.name, link=repo.url, desc=repo.description))
+        )
+
+    return template_array
+
+
+def connect_to_github():
+
+    if not getenv('GITHUB_USER') or not getenv('GITHUB_PASSWORD'):
+        raise EnvironmentError('GITHUB_USER & GITHUB_PASSWORD variables are missing.')
+
+    return Github(getenv('GITHUB_USER'), getenv('GITHUB_PASSWORD'))
