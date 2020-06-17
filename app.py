b3e6608c711ca0f28c160e02683537cc0d1c5bf3
ltabis <tab.rysak70@gmail.com>
2020-06-17 20:20:29 +0200

    :bug: fixed a bug with flask template rendering.

--- app.py
+++ app.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 def index():
 
     project_templates = create_project_templates()
+    static = render_template('index.html', projects=project_templates)
 
     with open('./index.html', 'w') as file:
-        file.write(render_template('index.html', projects=project_templates))
+        file.write(static)
 
+    return static
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
