c4ca78fe6101883746a9bbb6c5b48fe8a611fa6b
ltabis <tab.rysak70@gmail.com>
2020-06-17 20:16:44 +0200

    :arrow_up: Added images to projects.

--- backend/projects.py
+++ backend/projects.py
@@ -12,23 +12,23 @@ def create_project_templates():
 
     auth = connect_to_github()
 
-    projects = [
-        "ltabis/electron-dev-environment",
-        "matteovol/EclataxEngine",
-        "ltabis/raytracer",
-        "ltabis/Utility-scripts",
-        "ltabis/MinecraftBackupTool",
-        "ltabis/toDoJAVA",
-    ]
+    projects = {
+        "ltabis/electron-dev-environment": "assets/electron-logo.png",
+        "matteovol/EclataxEngine": "assets/cpp-logo.png",
+        "ltabis/raytracer": "assets/rust-logo.png",
+        "ltabis/toDoJAVA": "assets/java-logo.png",
+        "ltabis/Utility-scripts": "assets/py-logo.png",
+        "ltabis/MinecraftBackupTool": "assets/mc-logo.png",
+    }
 
     template_array = []
 
-    for project in projects:
+    for (project, image) in projects.items():
 
         repo = auth.get_repo(project)
 
         template_array.append(
-            Markup(render_template('project_card.html', title=repo.name, link=repo.html_url, desc=repo.description))
+            Markup(render_template('project_card.html', title=repo.name, link=repo.html_url, desc=repo.description, image=image))
         )
 
     return template_array
