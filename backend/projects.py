14e530a691c6e0f015ead3998b655ef03246ce71
ltabis <tab.rysak70@gmail.com>
2020-08-15 03:44:08 +0200

    :sparkles: added a dynamic language code badge generator.

--- backend/projects.py
+++ backend/projects.py
@@ -49,22 +49,29 @@ def create_project_templates():
 
 def create_project_html_template(repository, asset):
 
-    repo = auth.get_repo(repository)
-    color = ''
-    if languages[repo.language]["color"] is None:
-        color = '#111111'
-    else:
-        color = languages[repo.language]["color"]
-    language_text_color = '#ffffff' if is_dark(color[1:]) else '#111111'
+    repository = auth.get_repo(repository)
+    project_languages = repository.get_languages()
+    code_badges = []
 
+    for project_language in project_languages:
+        badge_color = ''
+        if "color" not in languages[project_language] or languages[project_language]["color"] is None:
+            badge_color = '#111111'
+        else:
+            badge_color = languages[project_language]["color"]
+        language_text_color = '#ffffff' if is_dark(badge_color[1:]) else '#111111'
+        code_badges.append((project_language, language_text_color, badge_color))
+
+    major_language_color = '#ffffff' if len(project_languages) == 0 else languages[repository.language]["color"]
     return Markup(render_template('project_card.html',
-        title=repo.name,
-        link=repo.html_url,
-        desc=repo.description,
+        badges=code_badges,
+        title=repository.name,
+        link=repository.html_url,
+        desc=repository.description,
         image=asset,
-        color=color,
-        language_name=repo.language,
-        language_url=repo.languages_url,
+        color=major_language_color,
+        language_name=repository.language,
+        language_url=repository.languages_url,
         language_text_color=language_text_color,
     ))
 
